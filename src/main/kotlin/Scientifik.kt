01d61a236e0e5fdbb04d6942389f16c5c6883d4f
Alexander Nozik <altavir@gmail.com>
2019-10-03 15:27:28 +0300

    Versions update

--- src/main/kotlin/Scientifik.kt
+++ src/main/kotlin/Scientifik.kt
@@ -2,8 +2,8 @@
  * Build constants
  */
 object Scientifik {
-    const val ioVersion = "0.1.14"
-    const val coroutinesVersion = "1.3.1"
+    const val ioVersion = "0.1.15"
+    const val coroutinesVersion = "1.3.2"
     const val atomicfuVersion = "0.12.11"
     const val serializationVersion = "0.13.0"
 }
