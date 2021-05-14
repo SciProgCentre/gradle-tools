5ff90436f51cc54a16f3a7355bd0d21d28dad4fd
Alexander Nozik <altavir@gmail.com>
2021-05-14 20:10:07 +0300

    update coroutines version

--- src/main/kotlin/ru/mipt/npm/gradle/KScienceVersions.kt
+++ src/main/kotlin/ru/mipt/npm/gradle/KScienceVersions.kt
@@ -8,7 +8,7 @@ import org.gradle.api.JavaVersion
 object KScienceVersions {
     const val kotlinVersion = "1.5.0"
     const val kotlinxNodeVersion = "0.0.7"
-    const val coroutinesVersion = "1.5.0-RC"
+    const val coroutinesVersion = "1.5.0"
     const val serializationVersion = "1.2.1"
     const val atomicVersion = "0.16.1"
     const val ktorVersion = "1.5.3"
