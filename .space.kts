a5618726bf7af1c38b87d11f8dd5cdd8fd1ff693
Alexander Nozik <nozik.aa@mipt.ru>
2022-07-10 09:01:20 +0000

    update publishing script

--- .space.kts
+++ .space.kts
@@ -6,5 +6,16 @@ job("Publish"){
     startOn {
         gitPush { enabled = false }
     }
-    gradlew("openjdk:11", "publishAllPublicationsToSpaceRepository")
+    container("openjdk:11") {
+        kotlinScript { api ->
+            api.space().projects.automation.deployments.start(
+                project = api.projectIdentifier(),
+                targetIdentifier = TargetIdentifier.Key("gradle-tools"),
+                version = "current",
+                // automatically update deployment status based on a status of a job
+                syncWithAutomationJob = true
+            )
+            api.gradlew("publishAllPublicationsToSpaceRepository")
+        }
+    }
 }
